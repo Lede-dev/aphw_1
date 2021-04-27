d8c2936a22b7315df0a555d8a2870bbf20875c67
CYMBAL-qwe <hee05008@naver.com>
2021-04-27 20:38:29 +0900

    주간 격자 토스트 메세지

--- app/src/main/java/com/example/aphw_1/fragments/WeekViewFragment.java
+++ app/src/main/java/com/example/aphw_1/fragments/WeekViewFragment.java
@@ -1,5 +1,6 @@
 package com.example.aphw_1.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
+import com.example.aphw_1.MainActivity;
 import com.example.aphw_1.adapters.WeekViewGridAdapter;
 import com.example.aphw_1.R;
+import com.example.aphw_1.data.CurrentTime;
+import com.example.aphw_1.utils.CalendarUtils;
 
 public class WeekViewFragment extends Fragment {
 
-    @Override
+
+    private static View tempView;
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
@@ -34,6 +43,28 @@ public class WeekViewFragment extends Fragment {
         // 그리드뷰에 어댑터 연결
         grid.setAdapter(gridAdapter);
 
+
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {   // 이 함수를 통해 이벤트 리스너 등록
+
+            @SuppressLint("WrongConstant")
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                Toast.makeText(MainActivity.getInstance(), "position=" + position , 0).show();      // toast message로 띄울 text
+
+                if (tempView == null) tempView = view; // 이전에 클릭한 뷰가 없다면 임시 뷰를 현재 뷰로 지정
+                tempView.setBackground(getResources().getDrawable(R.drawable.month_view_border_disable)); // 이전에 클릭한 뷰를 테두리 비활성화 상태로 변경
+                view.setBackground(getResources().getDrawable(R.drawable.month_view_border_enable)); // 클릭한 일의 배경 색을 변경
+                tempView = view; // 클릭한 뷰를 임시 뷰로 지정
+
+            }
+        });
+
         return fragment;
+
+
     }
+
+
 }
